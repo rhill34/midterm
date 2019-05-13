0ecf69761eb5ec6027dc7021132ee83994c49243
Robert Hill <rhill34@mail.greenriver.edu>
2019-05-13 13:24:12 -0700

    Step1.
    Define a route that displays a header and a link to the survey. -RH3

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+//Start a session
+session_start();
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Required file
+require_once('vendor/autoload.php');
+require_once('model/validate.php');
+
+//Instantiate Fat-Free
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define arrays
+$f3->set('meals', array('breakfast', 'lunch', 'dinner'));
+
+//Define a default route
+$f3->route('GET /', function() {
+
+    echo "<h1>Midterm Survey</h1>";
+    echo "<a href='order'>Take My Midterm Survey</a>";
+});
+
+//Define an order route
+$f3->route('GET|POST /order', function($f3) {
+
+    //If form has been submitted, validate
+    if(!empty($_POST)) {
+
+        //Get data from form
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+        $qty = $_POST['qty'];
+
+        //Add data to hive
+        $f3->set('food', $food);
+        $f3->set('meal', $meal);
+        $f3->set('qty',$qty);
+
+        //If data is valid
+        if (validForm()) {
+
+            //Write data to Session
+            $_SESSION['food'] = $food;
+            $_SESSION['meal'] = $meal;
+            $_SESSION['qty'] = $qty;
+
+            //Redirect to Summary
+            $f3->reroute('/summary');
+        }
+    }
+
+    //Display order form
+    $view = new Template();
+    echo $view->render('views/order-form.html');
+});
+
+//Define a summary route
+$f3->route('GET /summary', function() {
+
+    //Display summary
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+//Run Fat-Free
+$f3->run();
---
+++ model/validate.php
@@ -0,0 +1,86 @@
+<?php
+
+/* Validate the form
+ * @return boolean
+ */
+function validForm()
+{
+    global $f3;
+    $isValid = true;
+
+    if (!validFood($f3->get('food'))) {
+
+        $isValid = false;
+        $f3->set("errors['food']", "Please enter a food item");
+    }
+
+    if (!validQty($f3->get('qty'))) {
+
+        $isValid = false;
+        $f3->set("errors['qty']", "Please enter 1 or more");
+    }
+
+    if (!validMeal($f3->get('meal'))) {
+
+        $isValid = false;
+        $f3->set("errors['meal']", "Please select a meal");
+    }
+
+    if (!validCondiments($f3->get('cond'))) {
+
+        $isValid = false;
+        $f3->set("errors['cond']", "Invalid selection");
+    }
+
+    return $isValid;
+}
+
+/* Validate a food
+ * Food must not be empty and may only consist
+ * of alphabetic characters.
+ *
+ * @param String food
+ * @return boolean
+ */
+function validFood($food)
+{
+    return !empty($food) && ctype_alpha($food);
+
+}
+
+/* Validate quantity
+ * Quantity must not be empty and must be a number
+ * greater than 1.
+ *
+ * @param String qty
+ * @return boolean
+ */
+function validQty($qty)
+{
+    return (!empty($qty)
+        && ctype_digit($qty)
+        && (1 <= $qty));
+}
+
+/* Validate a meal
+ *
+ * @param String meal
+ * @return boolean
+ */
+function validMeal($meal)
+{
+    global $f3;
+    in_array($meal, $f3->get('meals'));
+    return true;
+}
+
+/* Validate condiments
+ *
+ * @param String cond
+ * @return boolean
+ */
+function validCondiments($cond)
+{
+    global $f3;
+    return true;
+}
